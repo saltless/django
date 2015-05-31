9f74556307134fb31ef0da564136ce8db56d49fc
saltless <saltles1@gmail.com>
2015-05-31 15:56:46 +0800

    Add model defination

--- BookManagement/books/models.py
+++ BookManagement/books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Publiser(models.Model):
+	name = models.CharField(max_length = 30)
+	address = models.CharField(max_length = 50)
+	city = models.CharField(max_length = 60)
+	state = models.CharField(max_length = 30)
+	country = models.CharField(max_length = 50)
+	website = models.URLField()
+
+class Author(models.Model):
+	firstName = models.CharField(max_length = 30)
+	lastName = models.CharField(max_length = 40)
+	email = models.EmailField()
+
+class Book(models.Model):
+	title = models.CharField(max_length = 100)
+	authors = models.ManyToManyField(Author)
+	publisher = models.ForeignKey(Publiser)
+	publicationData = models.DataField()
+
